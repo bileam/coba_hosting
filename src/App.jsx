c454b3e77ed1134ec25d78d4925a5cdf7fcdd87b
bileam mangalla <bileammangalla12@gmail.com>
2025-12-02 03:17:53 +0700

    update1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
-import Flex from "./Components/pembelajaran1/Flex";
-import DarkMode from "./Components/DarkMode/DarkMode";
-import Penampung from "./Components/Pages/Penampung";
+
 import Testing from "./Components/Pages/Testing";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      {/* <Flex /> */}
-      {/* <DarkMode /> */}
-      {/* <Penampung /> */}
-      <Testing />
-    </div>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Testing />} />
+    //   </Routes>
+    // </Router>
+    <Testing />
   );
 };
 
